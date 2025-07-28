580196235939963ad58c6312c8e850b7b8d2ff09
derek riemer <git@derekriemer.com>
2025-07-28 06:33:27 -0600

    Add ShortcutsTable model for managing shortcut data

---
+++ route_models.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from sqlmodel import Session
+
+from models.entity import Shortcut
+from helpers import get_shortcuts
+
+
+class ShortcutsTable(BaseModel):
+    filter: str = Field(default="")
+    page: int = Field(ge=0)
+    shortcuts: list[Shortcut] = Field()
+
+    @classmethod
+    # pylint: disable=W0622
+    def get_shortcuts_table(cls, session: Session, page: int = 0, filter: str = "") -> ShortcutsTable:
+        return ShortcutsTable(shortcuts=get_shortcuts(session, page=page, filter=filter), page=page, filter=filter)
